487e5d72dab495573bcc98b3a3fcacde71a65cdb
ShortyTapki <v.v.vinogradov99@gmail.com>
2022-09-26 21:08:27 +0300

    Started refactoring

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
-const fetchData = async () => {
+const sidebarItems = document.querySelectorAll(
+  '.col-1-description-sidebar__item'
+);
+
+const bigLamp = document.querySelector('.col-1-description__image');
+const smallLamp = document.querySelector('.col-2__lamp');
+
+sidebarItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    const id = item.dataset.id;
+    removeActiveClasses();
+    switch (id) {
+      case '2':
+        bigLamp.style.left = '33.4rem';
+        break;
+      case '3':
+        bigLamp.style.left = '23.1rem';
+        break;
+      default:
+        bigLamp.style.left = '29.4rem';
+    }
+    item.classList.add('col-1-description-sidebar__item--active');
+    bigLamp.setAttribute('src', `assets/lamp${id}.png`);
+    smallLamp.setAttribute('src', `assets/lamp${id}-medium.png`);
+    console.log(bigLamp.style.left);
+  });
+});
+
+function removeActiveClasses() {
+  sidebarItems.forEach((item) =>
+    item.classList.remove('col-1-description-sidebar__item--active')
+  );
+}
+
+async function fetchData() {
   const response = await fetch(
     'https://private-anon-b86aaf46c1-lampshop.apiary-mock.com/lamps',
     {
@@ -9,5 +43,7 @@ const fetchData = async () => {
   );
 
   const data = await response.json();
-  console.log(data);
-};
+  return data;
+}
+
+const data = fetchData();
