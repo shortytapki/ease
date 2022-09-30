014f4c058b46b8fcef070de61642cc616a563e92
ShortyTapki <v.v.vinogradov99@gmail.com>
2022-10-01 00:55:45 +0300

    moved lamp select to the right col

--- js/script.js
+++ js/script.js
@@ -1,36 +1,68 @@
-const sidebarItems = document.querySelectorAll(
-  '.col-1-description-sidebar__item'
-);
+const sidebarItems = document.querySelectorAll('.lamp-list__item');
+const bigLamp = document.querySelector('.description__image');
+const mediumLamp = document.querySelector('.right-col__image--medium');
+const sunBtn = document.querySelector('.btn--sun');
+const moonBtn = document.querySelector('.btn--moon');
+const heroImg = document.querySelector('.right-col__image');
+const initHeroImgSource = heroImg.getAttribute('src');
 
-const bigLamp = document.querySelector('.col-1-description__image');
-const smallLamp = document.querySelector('.col-2__lamp');
+let data = [];
+let src1 = '';
+let src2 = '';
+let src3 = '';
+let isDarkMode = false;
 
 sidebarItems.forEach((item) => {
   item.addEventListener('click', () => {
-    const id = item.dataset.id;
+    const id = item.children[0].dataset.id;
     removeActiveClasses();
     switch (id) {
       case '2':
-        bigLamp.style.left = '33.4rem';
+        bigLamp.style.left = '30rem';
+        bigLamp.setAttribute('src', src2);
+        setInitCond();
+        mediumLamp.setAttribute('src', src2);
         break;
       case '3':
         bigLamp.style.left = '23.1rem';
+        bigLamp.setAttribute('src', src3);
+        mediumLamp.setAttribute('src', src3);
+        mediumLamp.style.top = '-11rem';
+        mediumLamp.style.left = '24%';
+        isDarkMode = data.at;
         break;
       default:
         bigLamp.style.left = '29.4rem';
+        bigLamp.setAttribute('src', src1);
+        setInitCond();
+        mediumLamp.setAttribute('src', src1);
     }
-    item.classList.add('col-1-description-sidebar__item--active');
-    bigLamp.setAttribute('src', `assets/lamp${id}.png`);
-    smallLamp.setAttribute('src', `assets/lamp${id}-medium.png`);
-    console.log(bigLamp.style.left);
+    item.classList.add('lamp-list__item--active');
   });
 });
 
 function removeActiveClasses() {
   sidebarItems.forEach((item) =>
-    item.classList.remove('col-1-description-sidebar__item--active')
+    item.classList.remove('lamp-list__item--active')
   );
 }
+
+function setInitCond() {
+  mediumLamp.style.left = '25%';
+  mediumLamp.style.top = '0';
+  heroImg.setAttribute('src', initHeroImgSource);
+}
+
+moonBtn.addEventListener('click', () => {
+  mediumLamp.setAttribute('src', '');
+  heroImg.setAttribute('src', 'assets/webp/hero/hero-img-dark.webp');
+  heroImg.setAttribute(
+    'srcset',
+    `assets/webp/hero/hero-img-dark.webp    1x, 
+    assets/webp/hero/hero-img-dark@2x.webp 2x, 
+    assets/webp/hero/hero-img-dark@3x.webp 3x`
+  );
+});
 
 async function fetchData() {
   const response = await fetch(
@@ -46,4 +78,13 @@ async function fetchData() {
   return data;
 }
 
-const data = fetchData();
+async function setSources() {
+  data = await fetchData();
+  src1 = data.at(0).image;
+  src2 = data.at(2).image;
+  src3 = data.at(1).image;
+  mediumLamp.setAttribute('src', src1);
+  bigLamp.setAttribute('src', src1);
+}
+
+setSources();
